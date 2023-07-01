b5eddabd518f6b3424b99d951f8965d010eae044
William <whartanto1604@gmail.com>
2023-07-02 02:44:15 +0700

    komeng

--- src/app/pages/home/votes/votes.component.ts
+++ src/app/pages/home/votes/votes.component.ts
@@ -107,13 +107,13 @@ export class VotesComponent implements OnInit {
             username: user,
             org: sessionStorage.getItem('role')
           },
-          ballot: {
-            ballotId: uuid(),
-            electionId: this.currentElectionId,
-            voterId: user,
-            candidateId: _candidateId,
-            createdAt: new Date()
-          },    
+          // ballot: {
+          //   ballotId: uuid(),
+          //   electionId: this.currentElectionId,
+          //   voterId: user,
+          //   candidateId: _candidateId,
+          //   createdAt: new Date()
+          // },    
           token: sessionStorage.getItem('token')
         }
         this.voteService.castVote(requestBody).subscribe({
