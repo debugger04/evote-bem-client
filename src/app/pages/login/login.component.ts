43e30cb1904c939c97fe552d4b4364f9236a4aab
William <whartanto1604@gmail.com>
2023-07-09 01:49:14 +0700

    admin solved

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -13,8 +13,6 @@ import { v4 as uuid } from 'uuid';
 })
 export class LoginComponent implements OnInit {
 
-  firstLogin: boolean = false;
-
   loginForm: FormGroup = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', Validators.required),
@@ -61,52 +59,43 @@ export class LoginComponent implements OnInit {
         }
       },
       error: (err: any) => {
-        this.firstLogin = true;
-      },
-      complete: () => {
-        this.onErrorHandling();
+        Swal.fire(
+          'Sign In Failed!',
+          'Incorrect Username or Password',
+          'error'
+        );
       }
     });
   }
 
   onErrorHandling() {
-    if (this.firstLogin) {
-      const href: string = this.router.url;
-      if (href.includes('admin')) {
-        const requestBody = {
-          data : {
-            name: 'admin',
-            userID: uuid(),
-            email: 'admin@evote.bemft.com',
-            password: 'adminpw',
-            org: 'adminops',
-          }
+    const href: string = this.router.url;
+    if (href.includes('admin')) {
+      const requestBody = {
+        data : {
+          name: 'admin',
+          userID: uuid(),
+          email: 'admin@evote.com',
+          password: 'adminpw',
+          org: 'adminops',
         }
-        this.userService.register(requestBody).subscribe({
-          next: (res: any) => {
-            Swal.fire(
-              'Hi Admin!',
-              'We realize that this is your first time logging in, please try to login again after the page is reloaded',
-              'question'
-            );
-            window.location.reload();
-            return;
-          },
-          error: (err: any) => {
-            Swal.fire(
-              'Something\'s not right',
-              'Incorrect Username or Password',
-              'error'
-            );
-          }
-        });
       }
-      return;
+      this.userService.register(requestBody).subscribe({
+        next: (res: any) => {
+          Swal.fire(
+            'Hi Admin!',
+            'We realize that this is your first time logging in, please try to login again after reloading the page',
+            'question'
+          );
+        },
+        error: (err: any) => {
+          Swal.fire(
+            'Something\'s not right',
+            'Please re-check your admin configuration',
+            'error'
+          );
+        }
+      });
     }
-    Swal.fire(
-      'Sign In Failed!',
-      'Incorrect Username or Password',
-      'error'
-    );
   }
 }
