7abfef315223a55ab90dab262782ff14696b48cd
William <whartanto1604@gmail.com>
2023-07-09 02:20:15 +0700

    hapus alert

--- src/app/guard/route.guard.ts
+++ src/app/guard/route.guard.ts
@@ -26,11 +26,6 @@ export class RouteGuard implements CanActivate, CanActivateChild {
   private authorize(state: ActivatedRouteSnapshot): boolean {
     const authToken: boolean = sessionStorage.getItem('token') !== null;
     if (!authToken) {
-      Swal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text: 'Kamu belum ada akses untuk halaman ini!',
-      });
       this.redirect();
       return false;
     } else {
